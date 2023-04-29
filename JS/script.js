5888b2f33b70e709ff50d048e2ce751af8039701
Alexander Castman <alexandercastman@MacBook-Pro-som-tillhor-Alexander-2.local>
2023-04-29 20:07:42 +0200

    asdadsa

--- JS/script.js
+++ JS/script.js
@@ -1,42 +1,28 @@
-var data; // Deklarera variabeln data utanför onload-händelsen
 
-// Ladda in data från data.json
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'data.json', true);
-xhr.onload = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-        data = JSON.parse(xhr.responseText); // Tilldela värdet av data när det har laddats in
-        console.log(data); // för att se att data har laddats in korrekt
-    }
-};
-xhr.send(null);
+var data;
+
+fetch('data.json')
+    .then((response) => response.json())
+    .then((json) => data = json);
 
 // Metod som anropas när knapp 1 klickas på
 function handleButton1Click() {
-    // Slumpa ett patrullnummer, case och en stockholm_adress från objektet i data.json
+
     var patrullnummer = data.patrullnummer[Math.floor(Math.random() * data.patrullnummer.length)];
-    var cases = data.cases[Math.floor(Math.random() * data.cases.length)];
-    var stockholm_adresser = data.stockholm_adresser[Math.floor(Math.random() * data.stockholm_adresser.length)];
+    var caseText = data.cases[Math.floor(Math.random() * data.cases.length)];
+    var startAdress = data.stockholm_adresser[Math.floor(Math.random() * data.stockholm_adresser.length)];
+    var endAdress = data.stockholm_adresser[Math.floor(Math.random() * data.stockholm_adresser.length)];
 
-    // Sätt ihop de slumpmässiga värdena till en sträng
-    var text = "Patrullnummer: " + patrullnummer + "\n" +
-        "Case: " + cases + "\n" +
-        "Adress: " + stockholm_adresser;
 
-    // Visa den slumpmässiga strängen i den centrerade texten
-    var centeredText = document.getElementsByClassName("centered")[0];
-    centeredText.textContent = text;
+    var textElement = document.getElementById("centered-text");
+
+    var patrullText = "Du tjänstgör i " + patrullnummer + " och befinner dig på " + startAdress + " när du blir beordrade till " + endAdress + ".<br>"
+
+    textElement.innerHTML = patrullText + "<br>" + caseText + "<br>";
 }
 
-// Metod som anropas när knapp 2 klickas på
-function handleButton2Click() {
-    console.log("Knapp 2 klickades på!");
-    // Lägg till din egen logik här för vad som ska hända när knapp 2 klickas på
-}
 
 // Här kopplar vi metoderna till respektive knapp genom att hitta elementen med hjälp av DOM-manipulation
 var button1 = document.getElementsByClassName("button")[0];
-var button2 = document.getElementsByClassName("button")[1];
-
 button1.addEventListener("click", handleButton1Click);
-button2.addEventListener("click", handleButton2Click);
+
